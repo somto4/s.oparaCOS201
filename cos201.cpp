0f2bb60fb6707309946e2d07ffa39a669464ba5d
Somto Opara <somto@Somtos-MacBook-Pro.local>
2025-10-19 18:43:34 +0100

    week1-3

---
+++ cos201.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // double r = 20;
+    double r;
+    cin>>r;
+    double pi = 3.142;
+    double area = r*r*pi;
+    cout<<area;
+    return 0;
+}
